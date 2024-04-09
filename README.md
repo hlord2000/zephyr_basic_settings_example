2f32675b33db04df5078a9f31bfbef3ca2876ea4
Helmut Lord <kellyhlord@gmail.com>
2024-04-09 14:08:23 -0400

    basic settings demo
    Initial commit
    
    Signed-off-by: Helmut Lord <kellyhlord@gmail.com>

---
+++ README.md
@@ -0,0 +1,34 @@
+# Zephyr Basic Settings Demo
+#######################
+
+This sample demonstrates how to use the Zephyr settings subsystem for persistence across reboots. It also uses external flash as its non-volatile storage backend on supported boards.
+
+## Boards with External Flash Support
+- nRF5340DK
+- nRF52840DK
+
+In order to use external flash, define a partition as follows:
+```c
+/ {
+    chosen {
+        zephyr,settings-partition = &<YOUR_PARTITION_NODELABEL>;
+    };
+};
+
+&<YOUR_FLASH_NODE> {
+    partitions {
+        compatible = "fixed-partitions";
+        #address-cells = <1>;
+        #size-cells = <1>;
+
+        <YOUR_PARTITION_NODELABEL>: partition@0 {
+            label = "settings";
+            reg = <0x0 0x10000>;
+        };
+    };
+};
+```
+
+If a zephyr,settings-partition chosen node is not selected, the settings subsystem will automatically select a fixed partition with the label "storage."
+
+Other options are available for settings backends, including using a file system or internal circular buffer in flash. These options are not enabled in this example.
--- src/main.c
+++ src/main.c
@@ -1,6 +1,117 @@
 #include <zephyr/kernel.h>
+#include <zephyr/sys/reboot.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/settings/settings.h>
+
+LOG_MODULE_REGISTER(main);
+
+
+/* STEP 1: Let's create a structure that we want to save */
+struct my_settings_t {
+    uint32_t reboots;
+    uint32_t uptime;
+};
+
+struct my_settings_t my_value;
+
+/* STEP 2: Let's also use a plain atomic data type */
+uint32_t my_atomic_value = 0xCAFFE19E;
+
+/* STEP 4: Create our handler function, comparing the name of the setting */
+int settings_handle_set(const char *name, size_t len, settings_read_cb read_cb, void *cb_arg) {
+    const char *next;
+    int err;
+
+    if (settings_name_steq(name, "my_value", &next) && !next) {
+        if (len != sizeof(my_value)) {
+            return -EINVAL;
+        }
+
+        err = read_cb(cb_arg, &my_value, sizeof(my_value));
+        if (err <= 0) {
+            return err;
+        }
+
+        return 0;
+    }
+
+    else if (settings_name_steq(name, "my_atomic_value", &next) && !next) {
+        if (len != sizeof(my_atomic_value)) {
+            return -EINVAL;
+        }
+
+        err = read_cb(cb_arg, &my_atomic_value, sizeof(my_atomic_value));
+        if (err <= 0) {
+            return err;
+        }
+
+        return 0;
+    }
+
+    return -ENOENT;
+}
+
+/* STEP 3: Let us set our settings handler functions. For this example, we will only define how to set 
+ *   our different types of data.
+ */
+
+struct settings_handler my_conf = {
+    .name = "my_settings",
+    .h_set = settings_handle_set,
+};
 
 int main(void)
 {
-        return 0;
+    int err;
+    /* STEP 5: Initialize the settings subsystem */
+    err = settings_subsys_init();
+    if (err) {
+        LOG_ERR("settings_subsys_init failed (err %d)", err);
+    }
+
+    /* STEP 6: Register our settings configuration. Note: there are a number of
+     * different ways to accomplish this. You can define a handler statically, or
+     *  not use a handler at all
+     */
+    err = settings_register(&my_conf);
+    if (err) {
+        LOG_ERR("settings_register failed (err %d)", err);
+    }
+
+    /* STEP 7: We load all settings at boot with "settings_load" */
+    err = settings_load();
+    if (err) {
+        LOG_ERR("settings_load failed (err %d)", err);
+    }
+
+    k_msleep(5000);
+
+    LOG_INF("Reboots: %u, uptime: %u", my_value.reboots, my_value.uptime);
+    LOG_INF("Atomic value: 0x%08X", my_atomic_value);
+    
+    my_value.reboots++;
+    my_value.uptime = k_uptime_get_32();
+
+    if (my_value.reboots % 2) {
+        my_atomic_value = 0xDEADBEEF;
+    }
+    else {
+        my_atomic_value = 0xCAFFE19E;
+    }
+
+    /* STEP 8: Save the settings */
+    err = settings_save_one("my_settings/my_value", &my_value, sizeof(my_value));
+    if (err) {
+        LOG_ERR("Cannot save settings (err %d)", err);
+    }
+
+    err = settings_save_one("my_settings/my_atomic_value", &my_atomic_value, sizeof(my_atomic_value));
+    if (err) {
+        LOG_ERR("Cannot save settings (err %d)", err);
+    }
+
+    k_msleep(1000);
+
+    /* STEP 9: Reboot the system and see that the results persist */
+    sys_reboot(SYS_REBOOT_COLD);
 }
